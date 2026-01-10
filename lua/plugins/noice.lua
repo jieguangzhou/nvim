82b8e2a232f9a0ebf87e406561832e29ae42a5b2
JieguangZhou <jalonzjg@gmail.com>
2026-01-10 22:54:11 +0800

    Fix Noice cmdline highlight and disable smooth scroll

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.cmdline = opts.cmdline or {}
+      opts.cmdline.enabled = true
+      opts.cmdline.format = opts.cmdline.format or {}
+      -- Keep popup cmdline + highlight, but avoid TS query errors for vim cmdline.
+      opts.cmdline.format.cmdline = vim.tbl_extend(
+        "force",
+        opts.cmdline.format.cmdline or {},
+        { lang = "vim" }
+      )
+    end,
+    config = function(_, opts)
+      require("noice").setup(opts)
+      -- Force Noice to use Vim syntax (not Treesitter) for cmdline highlight.
+      local ts = require("noice.text.treesitter")
+      local orig_has_lang = ts.has_lang
+      ts.has_lang = function(lang)
+        if lang == "vim" then
+          return false
+        end
+        return orig_has_lang(lang)
+      end
+    end,
+  },
+}
