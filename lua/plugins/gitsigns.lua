8302e9b1a92135b04b5aeb27b6ca0b695e7342ea
JieguangZhou <jalonzjg@gmail.com>
2026-01-11 10:30:13 +0800

    fix: refresh gitsigns after git changes

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = function(_, opts)
+      opts.watch_gitdir = vim.tbl_deep_extend("force", opts.watch_gitdir or {}, {
+        enable = true,
+        interval = 1000,
+        follow_files = true,
+      })
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "BufWritePost" }, {
+        callback = function()
+          if package.loaded.gitsigns then
+            require("gitsigns").refresh()
+          end
+        end,
+      })
+    end,
+  },
+}
